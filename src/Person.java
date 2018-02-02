fd882931b1362a36a08913e9a498c0c936b6e1e2
Soham Dasgupta <sohamd@hotmail.com>
2018-02-02 11:05:57 -0600

    projects feature added

--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,32 @@
-
+import java.util.ArrayList;
 public class Person {
+	
 	private String name;
 	private int salary;
+	private ArrayList<String> projects;
 	
 	public Person()
 	{
 		name = "Soham Dasgupta";
 		salary = 25000;
+		projects = new ArrayList<String>();
+		projects.add("Finished the car factory report summary.");
 	}
 	
 	public void moreMoney()
 	{
 		salary +=1000;
+	}
+	
+	public String projectSummary()
+	{
+		String summ = "Projects List:";
+		for(int x = 0; x < projects.size(); x++)
+		{
+			summ+="\n"+projects.get(x);
+		}
+		
+		return summ;
 	}
 
 }
