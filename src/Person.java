31c8813add1cfb33fd0527c16404424207fc2b12
Reena <19yuanr@elmbrookstudents.org>
2018-02-02 11:06:43 -0600

    made occupation

--- src/Person.java
+++ src/Person.java
@@ -2,16 +2,24 @@
 public class Person {
 	private String name;
 	private int salary;
+	private String companyName;
+	private String jobTitle;
 	
 	public Person()
 	{
 		name = "Soham Dasgupta";
 		salary = 25000;
+		companyName = "a company";
+		jobTitle = "a title";
 	}
 	
 	public void moreMoney()
 	{
 		salary +=1000;
 	}
+	public String nameTag() {
+		return "Company name: " + companyName + "\nJob title: " + jobTitle;
+	}
+	
 
 }
--- src/TestClass.java
+++ src/TestClass.java
@@ -3,6 +3,8 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Person bob = new Person();
+		System.out.println(bob.nameTag());
 
 	}
 
