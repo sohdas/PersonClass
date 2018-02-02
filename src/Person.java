2dd766fbbe1e8c934f502a452a4fb7ff4728b669
Unknown <dragynmada@gmail.com>
2018-02-02 11:03:16 -0600

    Added Mailing Label and Address

--- src/Person.java
+++ src/Person.java
@@ -2,16 +2,20 @@
 public class Person {
 	private String name;
 	private int salary;
-	
-	public Person()
-	{
+	private String address;
+
+	public Person() {
 		name = "Soham Dasgupta";
 		salary = 25000;
+		address = "1234 Butter Way, Brookfield, WI, 53045";
+	}
+
+	public void moreMoney() {
+		salary += 1000;
 	}
 	
-	public void moreMoney()
-	{
-		salary +=1000;
+	public String mailingLabel() {
+		return name + "\n" + address.substring(0, address.indexOf(",")) + "\n" + address.substring(address.indexOf(",") + 2);
 	}
 
 }
